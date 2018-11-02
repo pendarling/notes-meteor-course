1c9eebd7b4fd987bb889647b6e14a8a124c8ae8d
Abby Zenger <zenger.abby@gmail.com>
2018-11-01 21:02:41 -0600

    Style note list

--- imports/ui/NoteListEmptyItem.js
+++ imports/ui/NoteListEmptyItem.js
@@ -4,9 +4,7 @@ import { createContainer } from 'meteor/react-meteor-data';
 
 const NoteListEmptyItem = () => {
   return (
-    <div>
-      <p>Add a note to begin! </p>
-    </div>
+      <p className='empty-item'>Create a note to get started! </p>
   );
 };
 
