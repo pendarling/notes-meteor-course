d16234b4f519f3eba1d8c9af447b657d5e0fe3b1
Abby Zenger <zenger.abby@gmail.com>
2018-11-02 22:50:28 -0600

    Setup mobile navigation

--- client/main.js
+++ client/main.js
@@ -10,19 +10,28 @@ import '../imports/startup/simple-schema-configuration.js';
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   const currentPagePrivacy = Session.get('currentPagePrivacy');
-  
+
   onAuthChange(isAuthenticated, currentPagePrivacy);
 });
 
 Tracker.autorun(() => {
   const selectedNoteId = Session.get('selectedNoteId');
+  Session.set('isNavOpen', false);
 
   if (selectedNoteId) {
     browserHistory.replace(`/dashboard/${selectedNoteId}`);
   }
 });
 
+Tracker.autorun(() => {
+  const isNavOpen = Session.get('isNavOpen');
+
+  document.body.classList.toggle('is-nav-open', isNavOpen);
+
+});
+
 Meteor.startup(() => {
   Session.set('selectedNoteId', undefined);
+  Session.set('isNavOpen', false);
   ReactDOM.render(routes, document.getElementById('app'));
 });
